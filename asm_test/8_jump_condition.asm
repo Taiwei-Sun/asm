aac00b52699b47f23f9dd2384c400a54412731d6
Taiwei-Sun <40155889+Taiwei-Sun@users.noreply.github.com>
2018-11-24 23:27:18 +0800

    test 8_

--- asm_test/8_jump_condition.asm
+++ asm_test/8_jump_condition.asm
@@ -8,19 +8,22 @@ section '.text' code readable executable
 
 start:
  
- mov eax,19
+ mov eax,0h
+ mov ecx,1h
+ sub ecx,1h
+ jz jz_0
+ 
  call print_eax
- jmp loop1
- inc eax
- call print_eax
-loop1:
- inc eax
+ jmp exit_9
+ 
+jz_1:
+ mov eax,1h
  call print_eax
  
  
 
 
- 
+exit_9:
  push 0
  call [ExitProcess]
 
